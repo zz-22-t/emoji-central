e9c45916fe4805c13fbce4507408451337060e2b
Giselle Morales <143736898+TLCGiselle@users.noreply.github.com>
2024-02-29 09:48:27 -0800

    Create GisM.js
    My emoji

---
+++ GisM.js
@@ -0,0 +1,46 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+ 
+ 
+  //face
+  
+ 
+  
+  noFill();
+  strokeWeight(10)
+  stroke("rgb(130,231,241)");
+  arc(200, 74, 299, 50, 0, 100);
+  stroke("black");
+  fill(238, 240, 113);
+  strokeWeight(4);
+  circle(200,150,200);
+  strokeWeight(8);
+  arc(200, 130, 120, 140, 120, 103);
+  strokeWeight(6);
+ 
+  
+  arc(160, 150, 50, 80, 211, 49.8);
+  arc(240, 150, 50, 80, 211, 49.8);
+
+  
+  strokeWeight(9)
+  noFill();
+  stroke("rgb(130,231,241)");
+  arc(200, 72, 299, 50,100,500);
+
+  
+ 
+ 
+  
+  
+  
+ 
+  
+ 
+  
+}
+
